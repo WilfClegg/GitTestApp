8bd340462ea9a412e7f73b4c2652790186b74bd7
WilfClegg <35201476+WilfClegg@users.noreply.github.com>
2023-04-23 11:43:08 -0400

    Initial Commit

---
+++ GitTestApp/GitTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestAppApp.swift
+//  GitTestApp
+//
+//  Created by Wilfred Lalonde on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct GitTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
